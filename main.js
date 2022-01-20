c50802442dbee92c69092cc7c208bf2747a40a4d
“Taylor <taytabbbb@aol.com>
2022-01-19 16:20:35 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-var endpoint = "https://api.npoint.io/75d14611840039c74201"
+var endpoint = "https://getpantry.cloud/apiv1/pantry/3d2c100e-3d1a-4fb5-9757-b36769125c06/basket/links"
+
+var simplecopy=function(){var d=document,b,cs,ss,f=false,n=d.createElement("DIV"),s=n.style;s.position="fixed";s.color="rgba(0,0,0,0)";function sc(t){if(!b){b=d.body};n.innerText=t;b.appendChild(n);ss=x(n);cs=d.execCommand("copy",f,null);b.removeChild(n);return ss&&cs};function x(n){var r,s,w=window.getSelection,c=b.createTextRange;if(c){r=c();r.moveToElementText(n);r.select();return !f}else if(w){s=w();r=d.createRange();r.selectNodeContents(n);s.removeAllRanges();s.addRange(r);return !f}else {return f}}return sc}();
 
 function getrandom() {
     var text = "";
@@ -29,7 +31,7 @@ function genhash(){
 function send_request(url) {
     this.url = url;
     $.ajax({
-        'url': endpoint + "/" + window.location.hash.substr(1),
+        'url': window.location.hash.substr(1),
         'type': 'POST',
         'data': JSON.stringify(this.url),
         'dataType': 'json',
